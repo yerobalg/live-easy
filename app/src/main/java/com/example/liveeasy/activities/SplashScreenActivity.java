dcbcb0d5c11a1bec94f095453a497eedb1160552
yerobalg <74844505+yerobalg@users.noreply.github.com>
2022-06-10 11:13:47 +0700

    added splash screen

---
+++ app/src/main/java/com/example/liveeasy/activities/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.liveeasy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.example.liveeasy.databinding.ActivitySplashScreenBinding;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ActivitySplashScreenBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        View decorView = getWindow().getDecorView();
+
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreenActivity.this,
+                    MainActivity.class));
+            finish();
+        }, 3000);
+    }
+}
